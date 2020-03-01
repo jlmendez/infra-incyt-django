18d6d0721dd7a84a074a6d96ca3acacb94cafba6
jlmendez <jlms793@hotmail.com>
2020-03-01 02:41:09 -0600

    funcional

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -21,32 +21,40 @@ class Choice(models.Model):
 	def __str__(self):
 		return self.choice_text
 
+class PollManager(models.Manager):
+    def with_counts(self):
+        from django.db import connection
+        with connection.cursor() as cursor:
+            cursor.execute("""select * from V_ISE2_INFRA_L2M""")
+            result_list = []
+            for row in cursor.fetchall():
+                p = self.model(id=row[0], infrasonido_1=row[1], infrasonido_2=row[2], infrasonido_3=row[3], infrasonido_4=row[4], fecha_recepcion=row[5])
+                result_list.append(p)
+        return result_list
+
 class ISE2_INFRA(models.Model):
-	ISE2_INFRA_id = models.IntegerField(default=0)
 	infrasonido_1 = models.FloatField(default=0)
 	infrasonido_2 = models.FloatField(default=0)
 	infrasonido_3 = models.FloatField(default=0)
 	infrasonido_4 = models.FloatField(default=0)
 	infrasonido_5 = models.FloatField(default=0)
-	posicion = models.CharField(max_length=200)
 	fecha_recepcion = models.DateTimeField('fecha_captura')
 
+	def ultimos_datos(self):
+		from django.db import connection
+		result_list = []
+		with connection.cursor() as cursor:
+			cursor.execute("""select * from V_ISE2_INFRA_L2M""")
+			for row in cursor.fetchall():
+				p = self.model(id=row[0], infrasonido_1=row[1], infrasonido_2=row[2], infrasonido_3=row[3], infrasonido_4=row[4], fecha_recepcion=row[5])
+				result_list.append(p)
+		return result_list
+
+
 	def __str__(self):
-		return self.fecha_recepcion	
+		return str(self.id)	
 	
 	def published_recently(self):
 		return self.fecha_recepcion >= timezone.now() - datetime.timedelta(minutes=1)
 
 
-class PollManager(models.Manager):
-    def with_counts(self):
-        from django.db import connection
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                select * from v_ise2_infr_Zdat""")
-            result_list = []
-            for row in cursor.fetchall():
-                p = self.model(id=row[0], question=row[1], poll_date=row[2])
-                p.num_responses = row[3]
-                result_list.append(p)
-        return result_list
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import datetime
+
+from math import pi
 from django.shortcuts import render, render_to_response
 from bokeh.plotting import figure, output_file, show #, ColumnDataSource
 from bokeh.models import ColumnDataSource
 from bokeh.embed import components
 from django.utils import timezone
+from bokeh.models import DatetimeTickFormatter
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -16,18 +19,41 @@ from .models import Choice, Question, ISE2_INFRA
 #def index(request):
 #	return HttpResponse("Hello world. You're at the polls index.")
 
+def ultimos_datos():
+		from django.db import connection
+
+		result_list = []
+		row_list = []
+		with connection.cursor() as cursor:
+			cursor.execute("""select * from V_ISE2_INFRA_L2M""")
+			for row in cursor.fetchall():
+				row_list = [row[0], row[1], row[2], row[3], row[4], row[5]]
+				result_list.append(row_list)
+		
+		return result_list
+
 def homepage(request):
 	#Graph X & Y coordinates
 
-
-	df = pd.DataFrame(list(ISE2_INFRA.objects.all().values('fecha_recepcion','infrasonido_1','infrasonido_2','infrasonido_3','infrasonido_4')[:10000]))
-	#df = pd.DataFrame(list(ISE2_INFRA.objects.filter(fecha_recepcion__lte=timezone.now()-datetime.timedelta(seconds=500)).values('fecha_recepcion','infrasonido_1','infrasonido_2','infrasonido_3','infrasonido_4'[:5000])))
-	source = ColumnDataSource(df)
-	plot = figure(title = 'Line Graph', x_axis_label = 'X-Axis', y_axis_label = 'Y-Axis', plot_width = 1000, plot_height = 400)
-	plot.line('fecha_recepcion', 'infrasonido_1', source=source, line_width=3, line_alpha=0.6)
-	plot.line('fecha_recepcion', 'infrasonido_2', source=source, line_width=3, line_alpha=0.3)
-	plot.line('fecha_recepcion', 'infrasonido_3', source=source, line_width=3, line_alpha=0.3)
-	plot.line('fecha_recepcion', 'infrasonido_4', source=source, line_width=3, line_alpha=0.3)
+	data_list = ultimos_datos()
+	frame = pd.DataFrame(data_list)
+	frame.columns= ['id','infrasonido_1','infrasonido_2','infrasonido_3','infrasonido_4','fecha_recepcion']
+	#df = pd.DataFrame(list(ISE2_INFRA.objects.all().values('fecha_recepcion','infrasonido_1','infrasonido_2','infrasonido_3','infrasonido_4')))
+	
+	source = ColumnDataSource(frame)
+	plot = figure(title = 'Line Graph', x_axis_label = 'X-Axis', y_axis_label = 'Y-Axis', x_axis_type='datetime', plot_width = 1000, plot_height = 400)
+	plot.line('fecha_recepcion', 'infrasonido_1', source=source, line_width=3, line_alpha=0.6, line_color="blue")
+	plot.line('fecha_recepcion', 'infrasonido_2', source=source, line_width=3, line_alpha=0.3, line_color="red")
+	plot.line('fecha_recepcion', 'infrasonido_3', source=source, line_width=3, line_alpha=0.3, line_color="pink")
+	plot.line('fecha_recepcion', 'infrasonido_4', source=source, line_width=3, line_alpha=0.3, line_color="green")
+	
+	#plot.xaxis.formatter=DatetimeTickFormatter(
+	#	hours=["%d %B %Y"],
+	#	days=["%d %B %Y"],
+	#	months=["%d %B %Y"],
+	#	years=["%d %B %Y"],
+	#)
+	plot.xaxis.major_label_orientation = pi/4
 	#plot.line(x,y,line_width = 2)
 	script, div = components(plot)
     
